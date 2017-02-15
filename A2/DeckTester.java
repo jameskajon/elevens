b2ead1c9b59d169e9aaefb7d9a6334addeb5576f
jimikajon <jimi.kajon@gmail.com>
2017-02-14 22:24:01 -0500

    A1 compleated but problem in testing class

--- A2/DeckTester.java
+++ A2/DeckTester.java
@@ -2,12 +2,27 @@
  * This is a class that tests the Deck class.
  */
 public class DeckTester {
-
-	/**
-	 * The main method in this class checks the Deck operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
-	}
+    
+    
+    static Deck deck = new Deck(new String[] {"Eight", "Jack", "Eight"}, new String[] {"Spades", "Hearts", "Spades"}, new int[] {8, 11, 8});
+    
+    /**
+     * The main method in this class checks the Card operations for consistency.
+     *  @param args is not used.
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Test Passed");
+        System.out.println("deal   : " + ((Boolean)(testDeal(0) && testDeal(1))).toString());
+    }
+    
+    
+    private static Boolean testDeal(int timeRan) {
+        
+        if (deck.deal().equals(deck.cards.get(2-timeRan))) {
+            return true;
+        }
+        return false;
+    }
+    
 }
